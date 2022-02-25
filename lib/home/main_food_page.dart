0d8bb54001b1d0e85c24a2408cabfbccfa125c91
Anbesha Thapa <anbesha@exmple.com>
2022-02-25 23:20:59 +0545

    first

---
+++ lib/home/main_food_page.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:fooddelivery/home/food_page_body.dart';
+import 'package:fooddelivery/utilities/colors.dart';
+import 'package:fooddelivery/widgets/Big_test.dart';
+import 'package:fooddelivery/widgets/small_test.dart';
+
+class MainFoodPage extends StatefulWidget {
+  const MainFoodPage({Key? key}) : super(key: key);
+
+  @override
+  _MainFoodPageState createState() => _MainFoodPageState();
+}
+
+class _MainFoodPageState extends State<MainFoodPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.only(top:45, bottom: 15),
+            padding: EdgeInsets.only(left:20, right: 20 ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  children: [
+                    BigText(text: "Nepal", color: AppColors.mainColor ),
+                   Row(
+                     children: [
+                       SmallText(text: "Kathmandu", color: Colors.black54,),
+                       Icon(Icons.arrow_drop_down_rounded)
+                     ],
+                   )
+                  ],
+                ),
+                Container(
+                  width: 45,
+                  height: 45,
+                  child: Icon(Icons.search, color: Colors.white,),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: AppColors.mainColor,
+                  ),
+                )
+              ],
+            ),
+          ),
+          FoodPageBody(),
+        ],
+      )
+    );
+  }
+}
